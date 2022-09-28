2012451ed9022a0a2f98500a6152a6d33283291b
Brian Hogan <brianhogan@napcs.com>
2022-09-28 16:32:41 -0500

    update starter and worker code to use Dial

--- start/main.go
+++ start/main.go
@@ -12,26 +12,33 @@ import (
 )
 
 func main() {
+
 	// Create the client object just once per process
-	c, err := client.NewClient(client.Options{})
+	c, err := client.Dial(client.Options{})
 	if err != nil {
 		log.Fatalln("unable to create Temporal client", err)
 	}
 	defer c.Close()
+
 	options := client.StartWorkflowOptions{
 		ID:        "greeting-workflow",
 		TaskQueue: app.GreetingTaskQueue,
 	}
+
+	// Start the Workflow
 	name := "World"
 	we, err := c.ExecuteWorkflow(context.Background(), options, app.GreetingWorkflow, name)
 	if err != nil {
 		log.Fatalln("unable to complete Workflow", err)
 	}
+
+	// Get the results
 	var greeting string
 	err = we.Get(context.Background(), &greeting)
 	if err != nil {
 		log.Fatalln("unable to get Workflow result", err)
 	}
+
 	printResults(greeting, we.GetID(), we.GetRunID())
 }
 
@@ -39,4 +46,5 @@ func printResults(greeting string, workflowID, runID string) {
 	fmt.Printf("\nWorkflowID: %s RunID: %s\n", workflowID, runID)
 	fmt.Printf("\n%s\n\n", greeting)
 }
+
 // @@@SNIPEND
--- worker/main.go
+++ worker/main.go
@@ -12,19 +12,22 @@ import (
 
 func main() {
 	// Create the client object just once per process
-	c, err := client.NewClient(client.Options{})
+	c, err := client.Dial(client.Options{})
 	if err != nil {
 		log.Fatalln("unable to create Temporal client", err)
 	}
 	defer c.Close()
+
 	// This worker hosts both Workflow and Activity functions
 	w := worker.New(c, app.GreetingTaskQueue, worker.Options{})
 	w.RegisterWorkflow(app.GreetingWorkflow)
 	w.RegisterActivity(app.ComposeGreeting)
+
 	// Start listening to the Task Queue
 	err = w.Run(worker.InterruptCh())
 	if err != nil {
 		log.Fatalln("unable to start Worker", err)
 	}
 }
+
 // @@@SNIPEND
